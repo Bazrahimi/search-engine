9785926aae00555ed887fcb4ee3a19d41bcf0af3
Baz Rahimi <bazrahimi@hotmail.com>
2023-11-21 02:58:04 +1100

    fixed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default {
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   open: true,
-  //   proxy: {
-  //     '/graphql': {
-  //       target: 'http://localhost:3001',
-  //       secure: false,
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
+  build: {
+    rollupOptions: {
+      external: ['graphql'],
+      // server: {
+      //   port: 3000,
+      //   open: true,
+      //   proxy: {
+      //     '/graphql': {
+      //       target: 'http://localhost:3001',
+      //       secure: false,
+      //       changeOrigin: true
+      //     }
+      //   }
+      // }
+      }
+  }
+
 };
