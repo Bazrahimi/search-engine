9fefe11b7db9b1c5b2520dd6a0e5bdccf75cc564
Baz Rahimi <bazrahimi@hotmail.com>
2023-11-21 11:33:25 +1100

    change vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,23 @@
-
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default {
   plugins: [react()],
+  server: {
+    // Specify server options here
+    port: 3000,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     rollupOptions: {
       external: ['graphql'],
-      // server: {
-      //   port: 3000,
-      //   open: true,
-      //   proxy: {
-      //     '/graphql': {
-      //       target: 'http://localhost:3001',
-      //       secure: false,
-      //       changeOrigin: true
-      //     }
-      //   }
-      // }
-      }
-  }
-
+    },
+  },
 };
